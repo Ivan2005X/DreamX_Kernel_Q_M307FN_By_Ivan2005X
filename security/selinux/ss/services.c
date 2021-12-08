3a649e48f4ff7cfd2828ca6baffe0292c0442fe4
Ivan2005X <ivanto1807@gmail.com>
2021-12-08 09:03:14 +0200

    Disable Audit Part 2

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3393,6 +3393,7 @@ out:
 	return match;
 }
 
+#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3415,6 +3416,7 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
+#endif
 
 #ifdef CONFIG_NETLABEL
 /**
